55145a6d668d0adb2ceb802eddc400531c785d7e
Dattatraya Chaskar <108402363+dattatray-searce@users.noreply.github.com>
2022-08-25 13:37:54 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace dotnetcore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchings"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
